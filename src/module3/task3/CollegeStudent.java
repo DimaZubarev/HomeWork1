e6dce5c393b513fdc0c62335d270df9ddf370cd8
Димон <1983zubarev@gmail.com>
2016-09-02 12:57:27 +0300

    all

--- src/module3/task3/CollegeStudent.java
+++ src/module3/task3/CollegeStudent.java
@@ -10,44 +10,19 @@ public class CollegeStudent extends Student {
         super(firstName, lastName, group);
     }
 
-    public CollegeStudent(int age, Course[] coursesTaken) {
-        super(age, coursesTaken);
+    public CollegeStudent(String lastName, Course[] coursesTaken) {
+        super(lastName, coursesTaken);
     }
 
-    public CollegeStudent(String firstName, String lastName, int group, int age, Course[] coursesTaken, String collegeName, int rating, long id) {
+    public CollegeStudent(String firstName, String lastName, int group, String collegeName, int rating, long id) {
         super(firstName, lastName, group);
         this.collegeName = collegeName;
         this.rating = rating;
         this.id = id;
-        this.setAge(age);
-        this.setCoursesTaken(coursesTaken);
-
-    }
-
-    public String getCollegeName() {
-        return collegeName;
-    }
-
-    public void setCollegeName(String collegeName) {
-        this.collegeName = collegeName;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
     }
 }
+
+
 
 
 
--- src/module3/task3/SpecialStudent.java
+++ src/module3/task3/SpecialStudent.java
@@ -5,35 +5,16 @@ public class SpecialStudent extends CollegeStudent {
     private long secretKey;
     private String email;
 
-    public SpecialStudent(String firstName, String lastName, int group) {
-        super(firstName, lastName, group);
+    public SpecialStudent(String lastName, Course[] coursesTaken) {
+        super(lastName, coursesTaken);
     }
 
-    public SpecialStudent(String firstName, String lastName, int group, int age, Course[] coursesTaken, String collegeName, int rating, long id) {
-        super(firstName, lastName, group, age, coursesTaken, collegeName, rating, id);
+    public SpecialStudent(String firstName, String lastName, int group, String collegeName, int rating, long id) {
+        super(firstName, lastName, group, collegeName, rating, id);
     }
 
-    public SpecialStudent(String firstName, String lastName, int group, int age, Course[] coursesTaken, String collegeName, int rating, long id, long secretKey, String email) {
-        super(firstName, lastName, group, age, coursesTaken, collegeName, rating, id);
+    public SpecialStudent(String firstName, String lastName, int group, String collegeName, int rating, long id, long secretKey) {
+        super(firstName, lastName, group, collegeName, rating, id);
         this.secretKey = secretKey;
-        this.email = email;
-    }
-
-    public long getSecretKey() {
-        return secretKey;
-    }
-
-    public void setSecretKey(long secretKey) {
-        this.secretKey = secretKey;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
-
-
