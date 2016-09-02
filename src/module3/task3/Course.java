23b172dee533fa7183507d51cac89ed8a7c89513
Димон <1983zubarev@gmail.com>
2016-09-02 12:45:02 +0300

    all

--- src/module3/task3/Course.java
+++ src/module3/task3/Course.java
@@ -15,40 +15,17 @@ public class Course {
         this.name = name;
     }
 
-    public Course(Date date, String student1, int hoursDuration, String teacherName) {
-        this.hoursDuration = hoursDuration;
-        this.teacherName = teacherName;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Course(String name, int hoursDuration, String teacherName) {
         this.name = name;
-    }
-
-    public int getHoursDuration() {
-        return hoursDuration;
-    }
-
-    public void setHoursDuration(int hoursDuration) {
         this.hoursDuration = hoursDuration;
-    }
-
-    public String getTeacherName() {
-        return teacherName;
-    }
-
-    public void setTeacherName(String teacherName) {
         this.teacherName = teacherName;
     }
+
+
 }
+
+
+
+
+
+
