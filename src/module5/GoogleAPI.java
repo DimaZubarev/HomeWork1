d18e6816116e2bb433791955063ea3423493f8a2
Димон <1983zubarev@gmail.com>
2016-09-16 11:13:41 +0300

    5.4

---
+++ src/module5/GoogleAPI.java
@@ -0,0 +1,23 @@
+package module5;
+
+import java.util.Date;
+
+public class GoogleAPI implements API {
+
+    private Room[] rooms;
+
+    public GoogleAPI() {
+
+        rooms = new Room[5];
+        rooms[0] = new Room(666777888, 120, 2, new Date(20160915), "Dnepr", "Kyiv");
+        rooms[1] = new Room(777888999, 130, 2, new Date(20160916), "Kyiv", "Kyiv");
+        rooms[2] = new Room(888999000, 160, 2, new Date(20160916), "Capital", "Kyiv");
+        rooms[3] = new Room(999000111, 125, 2, new Date(20160916), "Diplomat", "Kyiv");
+        rooms[4] = new Room(000111222, 200, 2, new Date(20160917), "Consul", "Kyiv");
+    }
+
+    @Override
+    public Room[] findRooms(int price, int persons, String city, String hotel) {
+        return new Room[3];
+    }
+}
---
+++ src/module5/TripAdvisorAPI.java
@@ -0,0 +1,23 @@
+package module5;
+
+import java.util.Date;
+
+public class TripAdvisorAPI implements API {
+
+    private Room[] rooms;
+
+    public TripAdvisorAPI() {
+
+        rooms = new Room[5];
+        rooms[0] = new Room(666777888, 100, 2, new Date(20160915), "Star", "Lviv");
+        rooms[1] = new Room(777888999, 110, 2, new Date(20160916), "Leon", "Lviv");
+        rooms[2] = new Room(888999000, 160, 2, new Date(20160916), "Galicia", "Lviv");
+        rooms[3] = new Room(999000111, 120, 2, new Date(20160916), "West", "Lviv");
+        rooms[4] = new Room(000111222, 125, 2, new Date(20160917), "Cossack", "Lviv");
+    }
+
+    @Override
+    public Room[] findRooms(int price, int persons, String city, String hotel) {
+        return new Room[1];
+    }
+}
