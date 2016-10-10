c30c1ae07aa4d549a66358a363e46aa42191d309
Димон <1983zubarev@gmail.com>
2016-10-10 18:06:51 +0300

    task1

---
+++ src/module7/task1/Currency.java
@@ -0,0 +1,5 @@
+package module7.task1;
+
+public enum Currency {
+    USD, UAH
+}
---
+++ src/module7/task1/Order.java
@@ -0,0 +1,80 @@
+package module7.task1;
+
+public class Order {
+
+    private long id;
+    private int price;
+    private Currency currency;
+    private String itemName;
+    private String shopIdentificator;
+    private User user;
+
+    public Order(long id, int price, Currency currency, String itemName, String shopIdentificator, User user) {
+        this.id = id;
+        this.price = price;
+        this.currency = currency;
+        this.itemName = itemName;
+        this.shopIdentificator = shopIdentificator;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getShopIdentificator() {
+        return shopIdentificator;
+    }
+
+    public void setShopIdentificator(String shopIdentificator) {
+        this.shopIdentificator = shopIdentificator;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", price=" + price +
+                ", currency=" + currency +
+                ", itemName='" + itemName + '\'' +
+                ", shopIdentificator='" + shopIdentificator + '\'' +
+                ", user=" + user +
+                '}';
+    }
+}
