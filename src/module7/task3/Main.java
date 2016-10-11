0570df0339f6b77544f118dc6f5b0caaaebb9602
Димон <1983zubarev@gmail.com>
2016-10-11 11:47:15 +0300

    task3

--- src/module7/task3/Main.java
+++ src/module7/task3/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         TreeSetController controller = new TreeSetController();
 
+        System.out.println(controller.findLastName("Petrov"));
         controller.orderWithLargestPrice();
+        controller.deleteForCurrency(Currency.USD);
+
     }
 }
