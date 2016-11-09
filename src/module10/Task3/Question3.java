40a01a1f8540aed7b0271fe35e8e26e613a51ec5
Димон <1983zubarev@gmail.com>
2016-11-09 17:07:44 +0200

    10.3

---
+++ src/module10/Task3/Question3.java
@@ -0,0 +1,16 @@
+package module10.Task3;
+
+import java.util.List;
+
+public class Question3 {
+    public static void main(String[] args) {
+
+        List<Integer> number = null;
+        try {
+            number.add(33);
+        }catch (NullPointerException e){
+            System.out.println("Нуливое значение: " + e);
+        }
+
+    }
+}
