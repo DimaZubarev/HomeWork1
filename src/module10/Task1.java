32d05dfcad30c3be2a38a2ce0f80eecc5d9a6222
Димон <1983zubarev@gmail.com>
2016-11-09 16:16:54 +0200

    10.1

---
+++ src/module10/Task1.java
@@ -0,0 +1,14 @@
+package module10;
+
+public class Task1 {
+    public static void main(String[] args) throws Throwable {
+        try {
+            throw new Exception("Exception");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("Finally");
+        }
+    }
+}
